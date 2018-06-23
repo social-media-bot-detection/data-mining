c088f1789ad337df4952682cdcef8edf88065a1b
carlosmartell97 <carlosmartell97@gmail.com>
2018-06-23 16:19:55 -0500

    now passing configuration file as parameter and showing additional follower information

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ubuntu:14.04
 RUN echo "creating data-mining image..."
 RUN mkdir mining
 ADD twitter_mining.py mining/twitter_mining.py
+ADD monitor_users.py mining/monitor_users.py
 ADD config_secret.json mining/config_secret.json
 
 RUN apt-get update
@@ -13,3 +14,4 @@ RUN pip install --upgrade pip
 RUN pip install --ignore-installed tweepy
 RUN pip install unidecode
 RUN pip install requests[security]
+RUN apt-get install nano
--- monitor_users.py
+++ monitor_users.py
@@ -1,30 +1,13 @@
 import sys
 import os
-import csv
 import json
 import tweepy
 import atexit
 import time
 import re
 from unidecode import unidecode
+import datetime
 
-
-# Configuration file
-abs_path = os.path.dirname(os.path.realpath(__file__)) + '/'
-with open("config_secret.json") as json_data_file:
-    data = json.load(json_data_file)
-
-
-# Twitter API credentials
-CONSUMER_KEY = data["consumer_key"]
-CONSUMER_SECRET = data["consumer_secret"]
-ACCESS_KEY = data["access_key"]
-ACCESS_SECRET = data["access_secret"]
-
-# authorize twitter, initialize tweepy
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
 emoji_pattern = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
@@ -83,22 +66,55 @@ def get_all_followers(screen_name):
     open_file(screen_name)
     atexit.register(close_file)
     first = True
-    for follower in tweepy.Cursor(api.followers, screen_name=screen_name).items():
+    users = tweepy.Cursor(api.followers, screen_name=screen_name, count=5000).items()
+
+    num_follower = 1
+    while True:
+        try:
+            follower = next(users)
+        except tweepy.TweepError:
+            time.sleep(60*15)
+            follower = next(users)
+        except StopIteration:
+            break
+        print "#%d %s <- %s" % (num_follower, screen_name, follower.screen_name)
         num_follower += 1
         if(num_follower >= 20000):
             num_follower = 1
             close_file()
             open_file(screen_name)
-        print "#%d %s <- %s" % (num_follower, screen_name, follower.screen_name)
         user = api.get_user(follower.screen_name)
         id = user.id_str
         description = unidecode(emoji_pattern.sub(r'', user.description))  # no emoji nor accents
         followers = user.followers_count
         following = user.friends_count
+        tweet_count = user.statuses_count
         profile_image_url = user.profile_image_url.replace("normal", "200x200")
         location = unidecode(user.location)
         verified = user.verified
-        created_at = str(user.created_at)
+        created_at = user.created_at
+
+        print "\tdescription: %s" % description
+        print "\tfollowers: %d" % followers
+        print "\tfollowing: %s" % following
+        print "\ttweet_count: %s" % tweet_count
+        print "\tlocation: %s" % location
+        current_time = datetime.datetime.now()
+        date_delta = current_time - created_at
+        created_at = str(created_at)
+        if(date_delta.days > 364):
+            date_delta_text = "{0:.1f} YEARS".format(date_delta.days/365.00)
+        elif(date_delta.days > 30):
+            date_delta_text = "{0:.1f} MONTHS".format(date_delta.days/30.00)
+        elif(date_delta.days > 0):  # more than a day
+            date_delta_text = "%d DAYS" % date_delta.days
+        elif(date_delta.seconds > 3599):  # more than an hour
+            date_delta_text = "{0:.1f} HOURS".format(date_delta.seconds/3600)
+        elif(date_delta.seconds > 59):  # more than a minute
+            date_delta_text = "{0:.1f} MINUTES".format(date_delta.seconds/60)
+        else:  # more than a second
+            date_delta_text = "%d SECONDS" % date_delta.seconds
+        print "\tcreated_at: %s, %s ago" % (created_at, date_delta_text)
 
         follower_saved = Follower(
             screen_name=follower.screen_name, id=id, description=description,
@@ -122,9 +138,25 @@ def real_user_id(screen_name):
 
 
 if __name__ == '__main__':
+    # pass in the configuration file
+    abs_path = os.path.dirname(os.path.realpath(__file__)) + '/'
+    with open(sys.argv[1], 'r') as json_data_file:
+        data = json.load(json_data_file)
+
+    # Twitter API credentials
+    CONSUMER_KEY = data["consumer_key"]
+    CONSUMER_SECRET = data["consumer_secret"]
+    ACCESS_KEY = data["access_key"]
+    ACCESS_SECRET = data["access_secret"]
+
+    # authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
     # pass in the username(s) of the account(s) you want to monitor
     ensure_dir("json/")
-    users = sys.argv[1:]
+    users = sys.argv[2:]
     for user in users:
         print "===== Examining %s =====" % user
 
